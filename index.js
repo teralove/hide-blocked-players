3cb6cf48b4c7ba0491c3317dae38977cda897f95
teralove <terascripts@gmail.com>
2018-07-05 17:47:28 -0400

    exception for battlegrounds

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 module.exports = function HideBlockedUsers(dispatch) {
     
-    let blockList;
+    let blockList = [],
+    inBg = false;
+    
+    dispatch.hook('S_LOAD_TOPO', 3, (event) => {
+        console.log('Load topo: ' + event.zone);
+        inBg = [116, 112].includes(event.zone) ? true : false;
+    });     
     
     dispatch.hook('S_USER_BLOCK_LIST', 2, (event) => {
         blockList = event.blockList;
     });   
     
     dispatch.hook('S_SPAWN_USER', 13, (event) => {
-        if (!blockList) return;
+        if (inBg) return;
         let player = blockList.find(p => p.name === event.name);
         if (player) return false;
     });       
